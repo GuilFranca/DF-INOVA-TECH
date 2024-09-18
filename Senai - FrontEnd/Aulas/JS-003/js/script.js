aa57a912f0b73bd0bb6a475dfa5dfadb594e4420
Guilherme França <131270776+GuilFranca@users.noreply.github.com>
2024-09-18 17:34:55 -0300

    Add files via upload

---
+++ Senai - FrontEnd/Aulas/JS-003/js/script.js
@@ -0,0 +1,20 @@
+let botaoNao = document.getElementById("nao");
+let botaoSim = document.getElementById("sim");
+let mensagemErro = document.getElementById("erro");
+
+botaoNao.addEventListener("mouseover", function() {
+
+    let posX = Math.random() * (window.innerWidth - botaoNao.offsetWidth);
+    let posY = Math.random() * (window.innerHeight - botaoNao.offsetHeight);
+
+    botaoNao.style.position = "absolute";
+    botaoNao.style.left = posX + "px";
+    botaoNao.style.top = posY + "px";
+
+    mensagemErro.style.display = "block";
+
+});
+
+botaoSim.addEventListener("click", function() {
+    alert("Ótima escolha! Vamos almoçar");
+})
